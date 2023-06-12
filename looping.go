adbcee12bb1245c6057112a39717ed6164f6cb51
dukeduk <ronald.yonatan@jec.co.id>
2023-06-12 21:29:24 +0700

    test looping

--- looping.go
+++ looping.go
@@ -1,8 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
+	iniLooping()
+}
+
+func iniLooping() {
 	for counter := 1; counter <= 15; counter++ {
 		if counter%3 == 0 && counter%5 == 0 {
 			fmt.Println("Fizzbuzz")
@@ -15,4 +22,18 @@ func main() {
 		}
 
 	}
+
+}
+
+func iniLoopingWitParameter(value int) string {
+	if value%3 == 0 && value%5 == 0 {
+		return "Fizzbuzz"
+	} else if value%5 == 0 {
+		return "buzz"
+	} else if value%3 == 0 {
+		return "fizz"
+	} else {
+		return strconv.Itoa(value)
+	}
+
 }
